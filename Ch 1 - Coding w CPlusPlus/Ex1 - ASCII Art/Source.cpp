56ad0910887b89502b0d8d704bd7713e9024d1ad
MarkPThomas <markums@gmail.com>
2016-03-01 18:28:23 -0800

    Completed second exercise
    All done!

---
+++ Ch 1 - Coding w CPlusPlus/Ex1 - ASCII Art/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		cout << "          ( )___( )" << endl;
+		cout << "          /__oo   \\ " << endl;
+		cout << "         ( \\/     )" << endl;
+		cout << "         | `=/    |" << endl;
+		cout << "        /         \\ " << endl;
+		cout << "       /  /    \\   \\ " << endl;
+		cout << "      /  (      \\   \\ " << endl;
+		cout << "     ( ,_/_      \\   \\ " << endl;
+		cout << "      \\_ '=       \\   )" << endl;
+		cout << "        \"\"'       /  /" << endl;
+		cout << "        ;        /  /'?" << endl;
+		cout << "        :       (((( /" << endl;
+		cout << "         `._   \\  _ (" << endl;
+		cout << "          __|   |  /_    " << endl;
+		cout << "        (\"__,..\"'_._.)   " << endl;
+		cout << " Give me a pic-u-nic basket or I'LL EAT YOU!!!" << endl;
+		return 0;
+}

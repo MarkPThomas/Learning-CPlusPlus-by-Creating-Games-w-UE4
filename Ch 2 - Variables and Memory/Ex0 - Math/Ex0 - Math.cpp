6688028c01c42f41f99a2b1119c110d5b6f41269
MarkPThomas <markums@gmail.com>
2016-03-02 21:14:38 -0800

    Completed exercise. Tada

--- Ch 2 - Variables and Memory/Ex0 - Math/Ex0 - Math.cpp
+++ Ch 2 - Variables and Memory/Ex0 - Math/Ex0 - Math.cpp
@@ -2,10 +2,44 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 int main()
 {
-    return 0;
+    // Yay math
+
+	int x = 4;
+	cout << x << endl;
+	x += 4;
+	cout << x << endl;
+	int y = 9;
+	cout << y << endl;
+	y -= 2;
+	cout << y << endl;
+	int z = 900;
+	cout << z << endl;
+	z /= 2;
+	cout << z << endl;
+	int a = 50;
+	cout << a << endl;
+	a *= 2;
+	cout << a << endl;
+	int b = 1;
+	cout << b << endl;
+	b += 1;
+	cout << b << endl;
+	int c = 2;
+	cout << c << endl;
+	c -= 200;
+	cout << c << endl;
+	int d = 5;
+	cout << d << endl;
+	d *= 5;
+	cout << d << endl;
+
+	return 0;
 }
 
